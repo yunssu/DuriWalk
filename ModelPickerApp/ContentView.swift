8e0a1581be3d43ae8f898ee44239e6d13e4f3eb2
RandomUserAPI <soneymarr@gmail.com>
2022-08-04 07:02:06 +0900

    Initial Commit

---
+++ ModelPickerApp/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ModelPickerApp
+//
+//  Created by 윤수 on 2022/08/04.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
